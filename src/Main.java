33be7afce2abf6f3cd044520e5492b0cfde45088
Kaye Ann <kayeann.ignacio@students.salle.url.edu>
2020-03-24 18:26:11 +0100

    Task LDPG3-63 implemented

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import lexic_analysis.Scanner;
 import model.Dictionary;
 import model.DictionaryManager;
+
+import java.io.FileNotFoundException;
 
 public class Main {
   public static void main(String[] args) {
       DictionaryManager dictionaryManager = new DictionaryManager();
       dictionaryManager.showList();
+      Scanner scanner = new Scanner();
+      scanner.readSourceCode();
 
   }
 }
--- src/lexic_analysis/Scanner.java
+++ src/lexic_analysis/Scanner.java
@@ -1,8 +1,37 @@
 package lexic_analysis;
 
-public class Scanner {
+import java.io.*;
 
+public class Scanner {
+  private static final String PATH="data/source_code.txt";
+
+  /**
+   * Reads source code file
+   */
+  public void readSourceCode() {
+    File source = new File(PATH);
+    String str = "";
+    BufferedReader br = null;
+
+    try {
+      br = new BufferedReader(new FileReader(source));
+    } catch (FileNotFoundException e) {
+      System.out.println("\nError, file can't be found.");
+      e.printStackTrace();
+    } finally {
+      while (true) {
+        try {
+          if ((str = br.readLine())!=null) {
+            System.out.println(str);
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
   public void removeSpecialCharacters(String[] delim) {
 
   }
+
 }
