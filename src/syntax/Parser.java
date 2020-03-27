0379fff452a425b75de0d6fa4c66ebe74f94c2a3
Nicole Marie Jimenez <nicolemarie.jimenez@students.salle.url.edu>
2020-03-28 00:17:20 +0100

    feat(LDPG3-44): Implement expression

--- src/syntax/Parser.java
+++ src/syntax/Parser.java
@@ -15,19 +15,36 @@ public class Parser {
     this.tokens = (LinkedList<Token>) tokens.clone();
     this.nextToken = this.tokens.getFirst();
 
+    expression();
+
     // If there is no other symbol left, the next token is EPSILON
-    if(nextToken.tokenType != TokenType.EPSILON) {
+    if (nextToken.tokenType != TokenType.EPSILON) {
       throw new ParserException("Unexpected symbol %s found", nextToken);
     }
   }
 
-  private void nextToken()
-  {
+  private void expression() {
+    oper_suma();
+  }
+
+  private void oper_suma() {
+    if (nextToken.tokenType == TokenType.ARITMETIC_SUM || nextToken.tokenType == TokenType.ARITMETIC_RES || nextToken.tokenType == TokenType.NUMBER) {
+      // sum_op -> PLUSMINUS term sum_op
+      nextToken();
+      //term();
+      oper_suma();
+    } else {
+      // sum_op -> EPSILON
+    }
+  }
+
+  private void nextToken() {
     tokens.pop();
     // at the end of input we return an epsilon token
-    if (tokens.isEmpty())
+    if (tokens.isEmpty()) {
       nextToken = new Token(TokenType.EPSILON, "");
-    else
+    } else {
       nextToken = tokens.getFirst();
+    }
   }
 }
