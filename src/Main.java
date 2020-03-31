b53367dfd6901af0ece131b0b2f6a3d9341a332b
kayeanni <38071786+kayeanni@users.noreply.github.com>
2020-03-31 20:56:56 +0200

    Task LDPG3-55 Checks if the word/lexeme exists in the dictionary if not, check the pattern. Once it's done, add to Symbol table.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
+import exceptions.ParserException;
 import lexic_analysis.Scanner;
 import model.CompilerManager;
+import syntax.Parser;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ParserException {
       CompilerManager dictionary = new CompilerManager();
       dictionary.showList();
       Scanner scanner = new Scanner();
+      Parser parser = new Parser();
+
       String[] words = scanner.generateTokens();
+      //El parser pide uno por uno los tokens
       for (int i = 0; i < words.length; i++) {
-          System.out.println(words[i]);
+          parser.getToken(words[i]);
       }
-
-
   }
 }
