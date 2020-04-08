bc56eea8efa2850af1f9a3f19f2a09d426f70280
Nicole Jimenez <nicolemarie.jimenez@students.salle.url.edu>
2020-04-08 16:05:13 +0200

    feat(LDPG3-75): Create IC classes

---
+++ src/intermediate/IntermediateCode.java
@@ -0,0 +1,11 @@
+package intermediate;
+
+public class IntermediateCode {
+  private Label label;
+  private ThreeAddrCode tac;
+
+  public IntermediateCode(Label label, ThreeAddrCode tac) {
+    this.label = label;
+    this.tac = tac;
+  }
+}
---
+++ src/intermediate/IntermediateCodeFlow.java
@@ -0,0 +1,11 @@
+package intermediate;
+
+import java.util.ArrayList;
+
+public class IntermediateCodeFlow {
+  private ArrayList<IntermediateCode> tacCodes;
+
+  public IntermediateCodeFlow() {
+    this.tacCodes = new ArrayList<IntermediateCode>();
+  }
+}
