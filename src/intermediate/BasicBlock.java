4108b4f45923c22ca71667b63217c79f6dd2b008
Carlos Mora Clavero <carlosmoracla@gmail.com>
2020-05-18 12:18:32 +0200

    log(LDPG3-96): Basic Block

--- src/intermediate/BasicBlock.java
+++ src/intermediate/BasicBlock.java
@@ -7,66 +7,75 @@ import java.util.List;
  * Represents a list of instructions to be executed sequentially
  */
 public class BasicBlock {
-  private static int counter = 0;
-  private String basicBlockId;
-  private IntermediateCode entryPoint;
-  private IntermediateCode exitPoint;
-  private LinkedList<IntermediateCode> instructions;
+    private static int counter = 0;
+    private String basicBlockId;
+    private IntermediateCode entryPoint;
+    private IntermediateCode exitPoint;
+    private LinkedList<IntermediateCode> instructions;
 
-  /**
-   *
-   * @param entry first instruccion in the basic block
-   */
-  public BasicBlock(IntermediateCode entry) {
-    basicBlockId = String.format("BB%d", counter++);
-    entryPoint = entry;
-    instructions = new LinkedList<>();
-    instructions.add(entry);
-  }
+    /**
+     * @param entry first instruccion in the basic block
+     */
+    public BasicBlock(IntermediateCode entry) {
+        basicBlockId = String.format("BB%d", counter++);
+        entryPoint = entry;
+        instructions = new LinkedList<>();
+        instructions.add(entry);
+    }
 
-  public BasicBlock() {
-    basicBlockId = String.format("BB%d", counter++);
-    instructions = new LinkedList<>();
-  }
+    public BasicBlock() {
+        basicBlockId = String.format("BB%d", counter++);
+        instructions = new LinkedList<>();
+    }
 
-  public void addInstruction(IntermediateCode intermediateCode) {
-    instructions.add(intermediateCode);
-  }
+    public void addInstruction(IntermediateCode intermediateCode) {
+        instructions.add(intermediateCode);
+    }
 
-  public void addInstructions(LinkedList<IntermediateCode> instructions) {
-    this.instructions = instructions;
-  }
+    public void addInstructions(LinkedList<IntermediateCode> instructions) {
+        this.instructions = instructions;
+    }
 
-  public IntermediateCode getNextInstruction() {
-    return instructions.getFirst();
-  }
+    public IntermediateCode getNextInstruction() {
+        return instructions.getFirst();
+    }
 
-  public IntermediateCode getEntryPoint() {
-    return entryPoint;
-  }
+    public IntermediateCode getEntryPoint() {
+        return entryPoint;
+    }
 
-  public void setEntryPoint(IntermediateCode entryPoint) {
-    this.entryPoint = entryPoint;
-    this.instructions.add(entryPoint);
-  }
+    public void setEntryPoint(IntermediateCode entryPoint) {
+        this.entryPoint = entryPoint;
+        this.instructions.add(entryPoint);
+    }
 
-  public IntermediateCode getExitPoint() {
-    return exitPoint;
-  }
+    public IntermediateCode getExitPoint() {
+        return exitPoint;
+    }
 
-  public void setExitPoint(IntermediateCode exitPoint) {
-    this.exitPoint = exitPoint;
-  }
+    public void setExitPoint(IntermediateCode exitPoint) {
+        this.exitPoint = exitPoint;
+    }
 
-  public String getBasicBlockId() {
-    return basicBlockId;
-  }
+    public String getBasicBlockId() {
+        return basicBlockId;
+    }
 
-  public void setBasicBlockId(String basicBlockId) {
-    this.basicBlockId = basicBlockId;
-  }
+    public void setBasicBlockId(String basicBlockId) {
+        this.basicBlockId = basicBlockId;
+    }
 
-  public LinkedList<IntermediateCode> getInstructions() {
-    return instructions;
-  }
+    public LinkedList<IntermediateCode> getInstructions() {
+        return instructions;
+    }
+
+    @Override
+    public String toString() {
+        return "BasicBlock{" +
+                "basicBlockId='" + basicBlockId + '\'' +
+                ", entryPoint=" + entryPoint +
+                ", exitPoint=" + exitPoint +
+                ", instructions=" + instructions +
+                '}';
+    }
 }
